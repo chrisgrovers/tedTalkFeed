431f3c9b130692d8d32ae56ec34a2d8026578e4c
chrisgrovers <chrisgrovers@gmail.com>
2015-11-30 22:35:57 -0800

    [new] added javascript.js

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// Please refer to the Media RSS feed at this URL:
+// http://feeds.feedburner.com/tedtalks_video
+
+// Please use the Google Feeds API to consume this feed entirely in the web
+// browser. See documentation here:
+
+// https://developers.google.com/feed/
+
+// Create a single web page which consumes this feed and presents it as an
+// attractive list of well-formatted feed entries, including item metadata.
+// When the user clicks on a particular feed entry in the list, they should be
+// presented with a "detail view" for that entry including more complete
+// metadata. Use your judgment to come up with a usable solution.
+
+// Do not write any server-side code. Use only HTML, CSS, and JavaScript for
+// this exercise. Feel free to make use of JavaScript libraries, etc. Be
+// advised that your code will be graded based on simplicity, maintainability,
+// and use of appropriate markup. Your design will also be graded based on
+// look and feel and overall user experience. Please produce a polished result
+// ready to be released to users.
+
